659ca1f003279609375365972621a75dce78decd
Yuvraj Singh <mr.uvsingh@gmail.com>
2023-06-24 14:58:05 +0530

    fourth commit

--- src/components/Form.js
+++ src/components/Form.js
@@ -94,7 +94,7 @@ const Form = ({users,setUsers}) => {
   };
 
   return (
-    <div className="min-h-[100vh] relative text-sky-50 bg-texture bg-cover bg-fixed flex justify-center items-center">
+    <div className="h-[100vh] relative text-sky-50 bg-texture bg-cover bg-fixed flex justify-center items-center">
     <button onClick={()=>navigate("/")} className="text-black border-2 z-10 border-amber-500 absolute top-6 left-10 bg-white p-2 rounded-2xl font-bold text-xl cursor-pointer">Users</button>
       <ToastContainer />
       <div className="w-full sm:w-5/6 md:w-2/3 h-auto bg-gray-700/[0.25] backdrop-blur-md rounded-xl shadow-2xl ">
